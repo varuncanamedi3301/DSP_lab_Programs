043b350bb137d87d83362470fd757d32dd8701b3
Varun Canamedi <68739986+varuncanamedi3301@users.noreply.github.com>
2021-12-16 12:41:09 +0530

    Add files via upload
    16th dec 2nd code commit

--- chebyshev_filter.m
+++ chebyshev_filter.m
@@ -1,21 +1,18 @@
-% EXPERIMENT 18. Design of high pass filter 
+% EXPERIMENT 21. Design of Chebyshev low pass type-1filter
+
 
 clc;
 clear all;
 close all;
 
-alphas = 50;
-alphap = 1;
+alphas = 0.9;
+alphap = 0.15;
 
-fpass = 1050;
-fstop = 600;   
-fsample = 3500;
+wp = 0.3*pi;
+ws = 0.5*pi;
 
-wp = 2*fpass/fsample;
-ws = 2*fstop/fsample;
-
-[n,wn] = buttord(wp,ws,alphap,alphas);
-[b,a] = butter(n,wn, 'high');
+[n,wn] = cheb1ord(wp/pi,ws/pi,alphap,alphas);
+[b,a] = cheby1(n,alphap,wn);
 [h,w] = freqz(b,a);
 
 subplot(2,1,1), plot(w/pi, 20*log10(abs(h)));
